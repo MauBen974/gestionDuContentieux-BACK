2a27d51777d46c462a3032e1ca7f6be4f7ad4027
Guillaume110 <guillaume.boussama@gmail.com>
2020-11-18 17:33:47 +0100

    microservice utilisateur

---
+++ microservice-utilisateur/src/main/java/com/adaming/repositories/UtilisateurRepository.java
@@ -0,0 +1,24 @@
+package com.adaming.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.adaming.entity.Utilisateur;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+
+	@Query("from Utilisateur u where u.email like :x and u.password like :y")
+    public Optional<Utilisateur> authentification(@Param("x") String email, @Param("y") String password);
+	
+	@Query("from Utilisateur u where u.archive like false")
+    public List<Utilisateur> findAllIfArchiveFalse();
+	
+	@Query("from Utilisateur u where u.idUtilisateur like :x and u.archive like false")
+    public Optional<Utilisateur> findOneIfAchiveFalse(@Param("x") Long id);
+}
