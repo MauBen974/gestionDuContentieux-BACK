a56bccaac85e23af23938eb660fc322d86933fbd
Maunier Benjamin <benjaminmaunierlr@gmail.com>
2020-11-18 14:36:55 +0100

    First Commit

---
+++ microservice-tache/src/main/java/com/adaming/MicroserviceTacheApplication.java
@@ -0,0 +1,13 @@
+package com.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTacheApplication.class, args);
+	}
+
+}
---
+++ microservice-tribunal/src/main/java/com/adaming/MicroserviceTribunalApplication.java
@@ -0,0 +1,13 @@
+package com.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTribunalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTribunalApplication.class, args);
+	}
+
+}
