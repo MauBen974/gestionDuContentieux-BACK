08ee3bd77048fb9907fa7e4c6854aa3748d17ae2
romainchnt <romainchanut@yahoo.fr>
2020-11-19 16:21:59 +0100

    changement requete repo (like --> is)

--- microservice-utilisateur/src/main/java/com/adaming/repositories/UtilisateurRepository.java
+++ microservice-utilisateur/src/main/java/com/adaming/repositories/UtilisateurRepository.java
@@ -16,7 +16,7 @@ public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> 
 	@Query("from Utilisateur u where u.email like :x and u.password like :y")
     public Optional<Utilisateur> authentification(@Param("x") String email, @Param("y") String password);
 	
-	@Query("from Utilisateur u where u.archive like false")
+	@Query("from Utilisateur u where u.archive is false")
     public List<Utilisateur> findAllIfArchiveFalse();
 	
 	@Query("from Utilisateur u where u.idUtilisateur like :x and u.archive like false")
