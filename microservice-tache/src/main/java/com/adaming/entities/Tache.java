3213cc7f8ce46a17037c15a3d0aad0e5db027948
romainchnt <romainchanut@yahoo.fr>
2020-11-20 15:34:05 +0100

    suppression cascade

--- microservice-tache/src/main/java/com/adaming/entities/Tache.java
+++ microservice-tache/src/main/java/com/adaming/entities/Tache.java
@@ -35,7 +35,7 @@ public class Tache implements Serializable {
 	private Long idUtilisateur;
 	private Long idTribunal;
 
-	@ManyToMany(cascade = { CascadeType.ALL })
+	@ManyToMany
 	@JoinTable(name = "Tache_Phase", joinColumns = { @JoinColumn(name = "idTache") }, inverseJoinColumns = {
 			@JoinColumn(name = "idPhase") })
 	private List<Phase> phases;
