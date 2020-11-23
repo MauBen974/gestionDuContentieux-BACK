79be70908fc008a1303029da8f00209bf57552fa
Guillaume110 <guillaume.boussama@gmail.com>
2020-11-23 11:11:36 +0100

    microservice tribunal test

--- microservice-tribunal/src/test/java/com/adaming/MicroserviceTribunalApplicationTests.java
+++ microservice-tribunal/src/test/java/com/adaming/MicroserviceTribunalApplicationTests.java
@@ -2,12 +2,20 @@ package com.adaming;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.adaming.servicetest.TribunalServiceMockTest;
 
 @SpringBootTest
 class MicroserviceTribunalApplicationTests {
 
+	TribunalServiceMockTest tribunalService = new TribunalServiceMockTest();
+
 	@Test
 	void contextLoads() {
+		tribunalService.createValidTribunal_ShouldReturnTribunalObject();
+		tribunalService.readValidTribunalById_shouldReturnThisTribunal();
+		tribunalService.readUnknownTribunalById_shouldReturnFalse();
+		tribunalService.readAllTribunalEmptyList_shouldReturnEmptyList();
+		tribunalService.readAllTribunalWithData_shouldReturnSize();
 	}
 
 }
--- microservice-tribunal/src/test/java/com/adaming/servicetest/TribunalServiceMockTest.java
+++ microservice-tribunal/src/test/java/com/adaming/servicetest/TribunalServiceMockTest.java
@@ -1,6 +1,9 @@
 package com.adaming.servicetest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.Collection;
@@ -8,9 +11,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 import com.adaming.entities.Tribunal;
 import com.adaming.repositories.TribunalRepository;
@@ -19,43 +26,64 @@ import com.adaming.services.TribunalServiceImpl;
 
 @SpringBootTest
 public class TribunalServiceMockTest {
-//	
-//	Tribunal tribunal;
-//	Long idTribunal = 1L;
-//	
-//	@Mock
-//	private TribunalRepository tribunalRepository;
-//	
-//	@InjectMocks
-//	private TribunalService tribunalService = new TribunalServiceImpl();
-//	
-//	//TEST FIND BY ID--------------------------------------
-//	@BeforeEach
-//	void setMockOutPut() {
-//		when(tribunalRepository.findById(idTribunal).get()).thenReturn(tribunal);
-//	}
-//	
-//	public void testFindOneById() {
-//		Tribunal result = tribunalService.findById(idTribunal).get();
-//		assertEquals(idTribunal,result.getIdTribunal());
-//	}
-//	
-//	//TEST FIND ALL----------------------------------------
-//	public void testFindAll() {
-//		assertEquals(Collection.class, tribunalService.findAll());
-//	}
-//
-//	//TEST SAVE--------------------------------------------
-//	public void testSave() {
-//		Tribunal tribunal = new Tribunal();
-//		assertEquals(Tribunal.class, tribunalService.save(tribunal));
-//	}
-//	
-//	//TEST DELETE------------------------------------------
-//	public void testDelete() {
-//		Tribunal tribunal = new Tribunal();
-//		tribunalService.delete(tribunal.getIdTribunal());
-//		assertEquals(null,tribunalService.findById(idTribunal).isEmpty());
-//	}
-	
+
+	@Autowired
+	TribunalService tribunalService;
+
+	@Test
+	@Sql(statements = "DELETE FROM Tribunal")
+	public void createValidTribunal_ShouldReturnTribunalObject() {
+		Tribunal tribunal = new Tribunal();
+		tribunal.setAdresse("test adresse");
+		tribunal.setEmail("test email");
+		tribunal.setTel(123456);
+		tribunal.setRegion("test region");
+		tribunal.setNomTribunal("test nom");
+
+		Tribunal retour = tribunalService.save(tribunal);
+		assertNotNull(retour);
+		assertEquals(retour.getEmail(), "valid@gmail.com");
+		assertFalse(retour.isArchive());
+	}
+
+	@Test
+	@Sql(statements = { "DELETE FROM tribunal",
+			"Insert INTO tribunal (idTribunal, adresse, email, tel, region, nomTribunal, archive)"
+					+ "VALUES(2,'adressetest', 'valid@gmail.com',123456,'region test', 'nom tribunal test', 0)" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	public void readValidTribunalById_shouldReturnThisTribunal() {
+		Tribunal tribunal = tribunalService.findById(2l).get();
+		assertEquals("adressetest", tribunal.getAdresse());
+		assertEquals("valid@gmail.com", tribunal.getEmail());
+		assertEquals(132456, tribunal.getTel());
+		assertEquals("region test", tribunal.getRegion());
+		assertEquals("nom tribunal test", tribunal.getNomTribunal());
+		assertFalse(tribunal.isArchive());
+	}
+
+	@Test
+	@Sql(statements = { "DELETE FROM tribunal",
+			"Insert INTO tribunal (idTribunal, adresse, email, tel, region, nomTribunal, archive)"
+					+ "VALUES(2,'adressetest', 'valid@gmail.com',123456,'region test', 'nom tribunal test', 0)" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	public void readUnknownTribunalById_shouldReturnFalse() {
+		assertFalse(tribunalService.findById(3l).isPresent());
+	}
+
+	@Test
+	@Sql(statements = "DELETE FROM tribunal")
+	public void readAllTribunalEmptyList_shouldReturnEmptyList() {
+		List<Tribunal> listAll = tribunalService.findAll();
+		assertTrue(listAll.isEmpty());
+	}
+
+	@Test
+	@Sql(statements = { "DELETE FROM tribunal",
+			"Insert INTO tribunal (idTribunal, adresse, email, tel, region, nomTribunal, archive)"
+					+ "VALUES(2,'adressetest', 'valid@gmail.com',123456,'region test', 'nom tribunal test', 0)",
+			"Insert INTO tribunal (idTribunal, adresse, email, tel, region, nomTribunal, archive)"
+					+ "VALUES(2,'adressetest2', 'second@gmail.com',741852,'deuxieme region test', 'deuxieme nom tribunal test', 0)" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+	public void readAllTribunalWithData_shouldReturnSize() {
+		List<Tribunal> listAll = tribunalService.findAll();
+		assertEquals(2, listAll.size());
+	}
+
 }
