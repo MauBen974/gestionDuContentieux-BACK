dc7ae4f95a02bb82f164a886e669d0b032c5e22b
Webs <webens@hotmail.fr>
2020-11-23 10:53:27 +0100

    commit apres premier pull

--- microservice-tache/src/main/java/com/adaming/MicroserviceTacheApplication.java
+++ microservice-tache/src/main/java/com/adaming/MicroserviceTacheApplication.java
@@ -1,59 +1,29 @@
 package com.adaming;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-<<<<<<< HEAD
-import com.adaming.entities.Affaire;
-import com.adaming.entities.Document;
-=======
+
+
 @SpringBootApplication
 @EnableSwagger2
 @EnableDiscoveryClient
 public class MicroserviceTacheApplication {
->>>>>>> b78c79aa5abda034ad6d4f3b08bab01f7dac0bd2
 
-import com.adaming.services.IAffaireService;
-import com.adaming.services.IDocumentService;
 
-@SpringBootApplication
-public class MicroserviceTacheApplication implements CommandLineRunner{
-	@Autowired
-	IDocumentService iDocumentService;
-	
-	@Autowired
-	IAffaireService iAffaireService;
+
+
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceTacheApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		DateFormat df=new SimpleDateFormat("dd/MM/yyyy");
-		
-		Affaire affaire1=iAffaireService.save(new Affaire("referenceAffaire1","titreAffaire1","descriptionAffaire1","ouvert"));
-		Affaire affaire2=iAffaireService.save(new Affaire("referenceAffaire2","titreAffaire2","descriptionAffaire2","en cours"));
-		Affaire affaire3=iAffaireService.save(new Affaire("ello","aloa","vacances","ouvert"));
-		Affaire affaire4=iAffaireService.save(new Affaire("holaquetal","bigmalion","vol","en cours"));
-		
-		Document doc1=iDocumentService.save(new Document((long)9,df.parse("12/01/1998"),"Nomdoc1","descriptiondoc1",true,affaire1));
-		Document doc2=iDocumentService.save(new Document((long)2,df.parse("25/01/1998"),"Nomdoc2","descriptiondoc2",true,affaire1));
-		Document doc3=iDocumentService.save(new Document((long)3,df.parse("12/01/1998"),"Nomdoc3","descriptiondoc2",false,affaire2));
-		Document doc4=iDocumentService.save(new Document((long)9,df.parse("12/01/1998"),"whales","miroir",true,affaire1));
-		Document doc5=iDocumentService.save(new Document((long)2,df.parse("25/01/1998"),"iguane","boisson",true,affaire1));
-		Document doc6=iDocumentService.save(new Document((long)3,df.parse("12/01/1998"),"pants","etoile",false,affaire2));
-
-		iAffaireService.findAll().forEach(c->{System.out.println(c.getReferenceAffaire());});
-		iDocumentService.findAll().forEach(c->{System.out.println(c.getNom());});
-	}
+	
 
 }
